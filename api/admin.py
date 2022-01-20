6bb858dfaaf5f116ff28e10065a4e08a94e9f64d
ilovedarknet <abboscik990@gmail.com>
2022-01-20 11:47:02 +0500

    rerun

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Booking)
+admin.site.register(Achievement)
+admin.site.register(AboutUs)
+admin.site.register(Card)
+
+
+
